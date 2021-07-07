50420311c56e507bb4f449f9157c3885f63ac58d
ZAF07 <zaffere07@gmail.com>
2021-07-08 00:35:33 +0800

    added production db

--- server/model/config/config.js
+++ server/model/config/config.js
@@ -17,9 +17,9 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     database: 'd8r60uah86jmrc',
-    username: 'zaffere',
+    username: 'ufmkahwjukklgd',
     host: 'ec2-54-227-246-76.compute-1.amazonaws.com',
-    password: null,
+    password: 'aa03f3c410f06c9fc94f28e95d4d074c0eba29340ba5553321a71e85f2a5f40f',
 
     dialectOptions: {
       ssl: { rejectUnauthorized: false },
