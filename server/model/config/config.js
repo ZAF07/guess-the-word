4939c6f4b67ee1f93a13db48a0c2f9decd676e47
ZAF07 <zaffere07@gmail.com>
2021-07-08 01:25:11 +0800

    added production db

--- server/model/config/config.js
+++ server/model/config/config.js
@@ -19,7 +19,7 @@ module.exports = {
     database: 'd8r60uah86jmrc',
     username: 'ufmkahwjukklgd',
     host: 'ec2-54-227-246-76.compute-1.amazonaws.com',
-    password: 'DATABASE_PASSWORD',
+    password: 'aa03f3c410f06c9fc94f28e95d4d074c0eba29340ba5553321a71e85f2a5f40f',
 
     dialectOptions: {
       ssl: { rejectUnauthorized: false },
