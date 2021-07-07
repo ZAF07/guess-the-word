e082e52ef31030546fda8ec33b4a888840c1a0cd
ZAF07 <zaffere07@gmail.com>
2021-07-08 00:04:09 +0800

    added production db

--- server/model/config/config.js
+++ server/model/config/config.js
@@ -16,6 +16,9 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    database: 'jumble_words',
+    username: 'zaffere',
+    password: null,
 
   },
 };
